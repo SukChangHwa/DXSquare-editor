258b910b61865996f445edc1c6192274edc0adde
이지현 <jhyi@inswave.com>
2023-12-19 13:46:58 +0900

    서식 선택 템플릿 수정

--- src/components/modal/DocumentSelectModal.vue
+++ src/components/modal/DocumentSelectModal.vue
@@ -5,10 +5,38 @@
     </template>
     <template #content>
       <div class="flex flex-col modal-content">
-        <div class="modal-list" v-for="(item, idx) of documentList" :key="idx">
-          <div class="p-2">
+        <div class="modal-list">
+          <!-- <div class="p-2">
             <button @click="confirm(item)">{{ item.fileName }}</button>
-          </div>
+          </div> -->
+          <table class="table-fixed border-spacing-2 w-full divide-y">
+            <thead>
+              <tr class="p-4 text-left">
+                <th class="hide">check</th>
+                <th class="p-2" width="400">서식 문서</th>
+                <th class="p-2">마지막 수정일</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, idx) of documentList"
+                :key="idx"
+                @click="selectItem(item)"
+                :class="[documentPicked === item.id ? 'hover:bg-zinc-200 hover:text-black bg-emerald-500 text-white' : 'hover:bg-zinc-200 hover:text-black']"
+              >
+                <td class="hide">
+                  <input
+                    type="radio"
+                    :id="'radio' + item.id"
+                    :value="item.id"
+                    v-model="documentPicked"
+                  />
+                </td>
+                <td class="p-2">{{ item.fileName }}</td>
+                <td class="p-2">2023-12-19 00:00:00</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </template>
@@ -28,6 +56,7 @@
 
 <script setup lang="ts">
 // TODO 저장된 문서가 없을 때 없다는 표시
+import { ref } from 'vue'
 import Modal from '@/components/ModalItem.vue'
 import { useDocStore, type ItemplateImg } from '@/stores/document'
 const props = defineProps({
@@ -35,6 +64,10 @@ const props = defineProps({
 })
 
 const documentStore = useDocStore()
+const documentPicked = ref('')
+const documentPickedItem = ref('')
+const selectedTrClass = ref('hover:bg-zinc-200 hover:text-black')
+
 let documentList: ItemplateImg[]
 
 documentList = documentStore.getAllDocuments()
@@ -45,9 +78,22 @@ const closeModal = () => {
   emit('cancel', false)
 }
 
-const confirm = (item) => {
-  emit('confirm', item)
+const confirm = () => {
+  debugger
+  emit('confirm', documentPickedItem.value)
 }
+
+const selectItem = (item) => {
+  debugger;
+  documentPicked.value = item.id
+  documentPickedItem.value = item
+  selectedTrClass.value += ' bg-emerald-500 text-white'
+}
+
 </script>
 
-<style scoped></style>
+<style scoped>
+.hide {
+  display: none;
+}
+</style>
